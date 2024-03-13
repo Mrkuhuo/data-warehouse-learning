8869ce293c92d14840d056d64afbfd48d5a71daa
WuZongYun <wzylzjtn@163.com>
2024-03-13 21:21:27 +0800

    seatunnel doris 代码提交

---
+++ src/main/java/org/bigdatatechcir/warehouse/doris/dml/ods_customer_login.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `ods_customer_login` (
+  `customer_id` bigint NOT NULL COMMENT '用户ID',
+  `login_name` varchar(128) NULL COMMENT '用户登录名',
+  `password` varchar(128) NULL COMMENT '密码',
+  `user_stats` tinyint NULL COMMENT '用户状态',
+  `modified_time`varchar(128) NULL COMMENT '最后修改时间'
+) ENGINE=OLAP
+UNIQUE KEY(`customer_id`)
+COMMENT '用户登录表'
+DISTRIBUTED BY HASH(`customer_id`) BUCKETS 1
+PROPERTIES (
+"replication_allocation" = "tag.location.default: 1",
+"in_memory" = "false",
+"storage_format" = "V2",
+"light_schema_change" = "true",
+"disable_auto_compaction" = "false"
+);
