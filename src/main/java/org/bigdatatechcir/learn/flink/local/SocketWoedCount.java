f007a1197ad4a89a3090479e115942c7eb2eb83a
WuZongYun <wzylzjtn@163.com>
2024-07-27 18:11:24 +0800

    提交代码

---
+++ src/main/java/org/bigdatatechcir/learn/flink/local/SocketWoedCount.java
@@ -0,0 +1,38 @@
+package org.bigdatatechcir.learn.flink.local;
+
+
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.RestOptions;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+public class SocketWoedCount {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+//设置WebUI绑定的本地端口
+        conf.setString(RestOptions.BIND_PORT,"8081");
+//使用配置
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
+
+//2.读取Socket数据
+        DataStreamSource<String> ds = env.socketTextStream("192.168.241.128", 8888);
+
+//3.准备K,V格式数据
+        SingleOutputStreamOperator<Tuple2<String, Integer>> tupleDS = ds.flatMap((String line, Collector<Tuple2<String, Integer>> out) -> {
+            String[] words = line.split(",");
+            for (String word : words) {
+                out.collect(Tuple2.of(word, 1));
+            }
+        }).returns(Types.TUPLE(Types.STRING, Types.INT));
+
+//4.聚合打印结果
+        tupleDS.keyBy(tp -> tp.f0).sum(1).print();
+
+//5.execute触发执行
+        env.execute();
+    }
+}
