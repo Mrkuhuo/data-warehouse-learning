2150e67bad357429a245b5a7a29fb39e8e484bca
WuZongYun <wzylzjtn@163.com>
2024-08-11 10:14:06 +0800

    提交代码

--- src/main/java/org/bigdatatechcir/learn/flink/SocketWordCountDemo.java
+++ src/main/java/org/bigdatatechcir/learn/flink/SocketWordCountDemo.java
@@ -13,16 +13,16 @@ import org.apache.flink.util.Collector;
 public class SocketWordCountDemo {
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-//设置WebUI绑定的本地端口
+        //设置WebUI绑定的本地端口
         conf.setString(RestOptions.BIND_PORT,"8081");
         conf.setString("rest.flamegraph.enabled","true");
-//使用配置
+        //使用配置
         StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
 
-//2.读取Socket数据
+        //2.读取Socket数据
         DataStreamSource<String> ds = env.socketTextStream("192.168.241.128", 8888);
 
-//3.准备K,V格式数据
+        //3.准备K,V格式数据
         SingleOutputStreamOperator<Tuple2<String, Integer>> tupleDS = ds.flatMap((String line, Collector<Tuple2<String, Integer>> out) -> {
             String[] words = line.split(",");
             for (String word : words) {
@@ -30,10 +30,10 @@ public class SocketWordCountDemo {
             }
         }).returns(Types.TUPLE(Types.STRING, Types.INT));
 
-//4.聚合打印结果
+        //4.聚合打印结果
         tupleDS.keyBy(tp -> tp.f0).sum(1).print();
 
-//5.execute触发执行
+        //5.execute触发执行
         env.execute();
     }
 }
